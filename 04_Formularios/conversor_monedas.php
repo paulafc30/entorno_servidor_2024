a3bcce954aaa9e687b1c630116c58e3a85292ed5
paulafc30 <paulafc30@gmail.com>
2024-10-16 01:26:26 +0200

    Repaso

--- 04_Formularios/conversor_monedas.php
+++ 04_Formularios/conversor_monedas.php
@@ -12,30 +12,57 @@
 <body>
     <!-- Convertir monedas--> 
     <form action="" method="post">
-        <label for="precio">Precio</label>
-        <input type="text" name="precio" id="precio">
-        <br><br>
-        <select name="iva">
-            <option value="general">General</option>
-            <option value="reducido">Reducido</option>
-            <option value="superreducido">Superreducido</option>
-        </select>
-        <input type="submit" value="Calcular">
-    </form>
+            <label for="monedaA">Moneda inicial</label>
+            <input type="number" name="monedaA" id="monedaA">
+            <br><br>
+            <select name="moneda_ini">
+                <option value="dolares">Dolares</option>
+                <option value="yenes">Yenes</option>
+                <option value="euros">Euros</option>
+            </select>
+            <br><br>
+            <label for="monedaA">Moneda final</label>
+            <select name="moneda_fin">
+                <option value="dolares">Dolares</option>
+                <option value="yenes">Yenes</option>
+                <option value="euros">Euros</option>
+            </select>
+            <br><br>
+            <input type="submit" value="Convertir">
+        </form>
 
-    <?php 
+        <?php 
         if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $precio = $_POST["precio"];
-            $iva = $_POST["iva"];
+            $monedaA = $_POST["monedaA"];
+            $moneda_ini = $_POST["moneda_ini"];
+            $moneda_fin = $_POST["moneda_fin"];
 
-
-            $pvp = match($iva){
-                "general" => $precio * 1.21,
-                "reducido" => $precio * 1.1,
-                "superreducido" => $precio * 1.04,
-            };
-        echo "<h1>El PVP seria: $pvp</h1>";
+            $tasas_conversion = [
+                "dolares" => ["dolares" => 1, "yenes" => 149.7, "euros" => 0.95],
+                "yenes" => ["dolares" => 0.0067, "yenes" => 1, "euros" => 0.0064],
+                "euros" => ["dolares" => 1.05, "yenes" => 156.7, "euros" => 1],
+            ];
+    
+            if ($moneda_ini == "dolares" && $moneda_fin == "euros") {
+                $tasa = 0.95;
+            } elseif ($moneda_ini == "dolares" && $moneda_fin == "yenes") {
+                $tasa = 149.7;
+            } elseif ($moneda_ini == "euros" && $moneda_fin == "dolares") {
+                $tasa = 1.05;
+            } elseif ($moneda_ini == "euros" && $moneda_fin == "yenes") {
+                $tasa = 156.7;
+            } elseif ($moneda_ini == "yenes" && $moneda_fin == "dolares") {
+                $tasa = 0.0067;
+            } elseif ($moneda_ini == "yenes" && $moneda_fin == "euros") {
+                $tasa = 0.0064;
+            } else {
+                $tasa = 1; // Si las monedas son iguales
+            }
+            
+            $resultado = $monedaA * $tasa;
+    
+            echo "<h1>$monedaA $moneda_ini son equivalentes a $resultado $moneda_fin.</h1>";
         }
-    ?>
+        ?>
 </body>
 </html>
