d6263a46201b5c7ecf6e7fbebebf02f76fa27110
paulafc30 <paulafc30@gmail.com>
2024-11-06 11:30:43 +0100

    Clase 06/11

---
+++ 04_Formularios/futbol.php
@@ -0,0 +1,124 @@
+<!--Malaga C.F
+Equipos de la liga
+- Nombre (letras con tilde, ñ, espacios en blanco y punto)
+- Inicial (3 letras)
+- Liga (select con las opciones. Liga EA Sports, Liga Hypermotion, Liga Primera RFEF)
+- Ciudad (letras con tilde, ñ, ç y espacios en blanco)
+- Tiene nombre liga (select con si o no)
+- Fecha de fundacion (entre hoy y el 18 de diciembre de 1889)
+- Numero de jugadores (entre 22 y 32)
+-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Futbol</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );  
+        require('../04_Formularios/depurar.php');
+    ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $tmp_nombre = $_POST["nombre"];
+            $tmp_inicial = $_POST["inicial"];
+            $tmp_liga = $_POST["liga"];
+            $tmp_ciudad = $_POST["ciudad"];
+            $tmp_titulos = $_POST["titulos"];
+            $tmp_fecha = $_POST["fecha"];
+            $tmp_jugadores = $_POST["jugadores"];
+
+            if($tmp_nombre == '') {
+                $err_nombre = "El nombre es obligatorio";
+            } else {
+                $patron = "/^[a-zA-Z0-9_\-.+]+@([a-zA-Z0-9-]+.)+[a-zA-Z]+$/";
+                if(!preg_match($patron, $tmp_correo)) {
+                    $err_nombre = "El correo no es válido";
+                } else {
+                    $palabras_baneadas = ["caca","peo","recorcholis","caracoles","repampanos"];
+
+                    $palabras_encontradas = "";
+                    foreach($palabras_baneadas as $palabra_baneada) {
+                        if(str_contains($tmp_correo,$palabra_baneada)) {
+                            $palabras_encontradas = "$palabra_baneada, " . $palabras_encontradas;
+                        }
+                        if($palabras_encontradas != '') {
+                            $err_correo = "No se permiten las palabras: $palabras_encontradas";
+                        } else {
+                            $correo = $tmp_correo;
+                        }
+                    }
+                }
+            }
+        }
+    ?>
+    <div class="container">
+
+        <h1>Formulario de futbol</h1>
+
+        <form class="col-4" action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">nombre:</label>
+                <input class="form-control" type="text" name="nombre">
+                <?php if(isset($err_nombre)) echo "<span class='error'>$err_nombre</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Inicial:</label>
+                <input class="form-control" type="text" name="inicial">
+                <?php if(isset($err_inicial)) echo "<span class='error'>$err_inicial</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Liga:</label>
+                <select name="liga">
+                    <option value="ligaea">Liga EA Sports</option>
+                    <option value="ligahyper">Liga Hypermotion</option>
+                    <option value="ligaprimera">Liga Primera RFEF</option>
+                </select>
+                <?php if(isset($err_liga)) echo "<span class='error'>$err_liga</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Ciudad:</label>
+                <input class="form-control" type="text" name="ciudad">
+                <?php if(isset($err_ciudad)) echo "<span class='error'>$err_ciudad</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Tiene nombres?:</label>
+                <select name="nombres_liga">
+                    <option value="si">Si</option>
+                    <option value="no">No</option>
+                </select>
+                <?php if(isset($err_titulos)) echo "<span class='error'>$err_titulos</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Fecha de fundacion:</label>
+                <input class="form-control" type="date" name="fecha">
+                <?php if(isset($err_fecha)) echo "<span class='error'>$err_fecha</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <label class="form-label">Numero jugadores:</label>
+                <input class="form-control" type="text" name="jugadores">
+                <?php if(isset($err_jugadores)) echo "<span class='error'>$err_jugadores</span>" ?>
+            </div> <br>
+
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- 04_Formularios/videojuegos.php
+++ 04_Formularios/videojuegos.php
@@ -13,48 +13,113 @@ fecha de lanzamiento = el video juego más antiguo admisible será del 1 de ener
 */-->
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Videojuegos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );  
+    ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-    <h2>Formulario de Videojuego</h2>
-  
-    <form  action="" method="post">
-        <label for="titulo">Título: </label>
-        <input type="text" id="titulo" name="titulo"><br><br>
-
-        <label>Consola:</label>
-        <input type="radio" name="consola" value="Nintendo Switch"> Nintendo Switch
-        <input type="radio" name="consola" value="PS5"> PS5
-        <input type="radio" name="consola" value="PS4"> PS4
-        <input type="radio" name="consola" value="Xbox Series X/S"> Xbox Series X/S<br><br>
-
-        <label for="fecha">Fecha de lanzamiento:</label>
-        <input type="date" id="fecha" name="fecha" ><br><br>
-
-        <label for="pegi">PEGI:</label>
-        <select id="pegi" name="pegi">
-        <option value="3">3</option>
-        <option value="7">7</option>
-        <option value="12">12</option>
-        <option value="16">16</option>
-        <option value="18">18</option>
-        </select><br><br>
-
-        <label for="descripcion">Descripción:</label><br>
-        <textarea id="descripcion" name="descripcion"></textarea><br><br>
-
-        <button type="submit" name="submit">Enviar</button>
-    </form>
     <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST["consola"])) {
+            $tmp_consola = $_POST["consola"];
+        } else {
+            $tmp_consola = "";
+        }
+        $tmp_fecha_lanzamiento = $_POST["fecha_lanzamiento"];
+
+        if($tmp_consola == '') {
+            $err_consola = "La consola es obligatoria";
+        } else {
+            $consolas_validas = ["ps4","ps5","switch","xboxsx"];
+            if(!in_array($tmp_consola,$consolas_validas)) {
+                $err_consola = "La consola no es válida";
+            } else {
+                $consola = $tmp_consola;
+            }
+        }
+
+        if($tmp_fecha_lanzamiento == '') {
+            $err_fecha_lanzamiento = "La fecha de lanzamiento es obligatoria";
+        } else {
+            $patron = "/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/";
+            if(!preg_match($patron, $tmp_fecha_lanzamiento)) {
+                $err_fecha_lanzamiento = "Formato de fecha incorrecto";
+            } else {
+                list($anno_lanzamiento,$mes_lanzamiento,$dia_lanzamiento) =
+                    explode('-',$tmp_fecha_lanzamiento);
+                if($anno_lanzamiento < 1947) {
+                    $err_fecha_lanzamiento = "El año no puede ser anterior a 1947";
+                } else {
+                    $anno_actual = date("Y");
+                    $mes_actual = date("m");
+                    $dia_actual = date("d");
+
+                    if($anno_lanzamiento - $anno_actual < 5) {
+                        $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                    } elseif($anno_lanzamiento - $anno_actual > 5) {
+                        $err_fecha_lanzamiento = "La fecha debe ser anterior a dentro de 5 años";
+                    } elseif($anno_lanzamiento - $anno_actual == 5) {
+                        if($mes_lanzamiento - $mes_actual < 0) {
+                            $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                        } elseif($mes_lanzamiento - $mes_actual > 0) {
+                            $err_fecha_lanzamiento = "La fecha debe ser anterior a dentro de 5 años";
+                        } elseif($mes_lanzamiento - $mes_actual == 0) {
+                            if($dia_lanzamiento - $dia_actual <= 0) {
+                                $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                            } elseif($dia_lanzamiento - $dia_actual > 0) {
+                                $err_fecha_lanzamiento = "La fecha debe ser anterior a dentro de 5 años";
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-    
     ?>
+    <div class="container">
+        <h1>Formulario de videojuegos</h1>
+        <form class="col-4" action="" method="post">
+            <div class="mb-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="consola" value="ps4">
+                    <label class="form-check-label">Playstation 4</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="consola" value="ps5">
+                    <label class="form-check-label">Playstation 5</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="consola" value="switch">
+                    <label class="form-check-label">Nintendo Switch</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="consola" value="xboxsx">
+                    <label class="form-check-label">XBox Series X/S</label>
+                </div>
+                <?php if(isset($err_consola)) echo "<span class='error'>$err_consola</span>" ?>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Fecha de lanzamiento</label>
+                <input class="form-control" type="date" name="fecha_lanzamiento">
+                <?php if(isset($err_fecha_lanzamiento)) echo "<span class='error'>$err_fecha_lanzamiento</span>" ?>
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
